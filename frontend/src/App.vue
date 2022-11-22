8c64390996274bd268ba8b2e1af61be71dcaab08
cyp0633 <cyp0633@cyp0633.icu>
2022-11-22 19:38:31 +0800

    Generate overall layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
-
+import { NH1, NLayout, NLayoutContent, NLayoutHeader, NLayoutSider } from 'naive-ui'
 const { t, availableLocales: languages, locale } = useI18n();
+import ConversationItemVue from "./components/ConversationItem.vue";
+import FromConversationVue from "./components/FromConversation.vue";
+import ToConversationVue from "./components/ToConversation.vue";
 
-const onclickLanguageHandle = (item: string) => {
-  item !== locale.value ? (locale.value = item) : false;
-};
-
-const onclickMinimise = () => {};
-
-const onclickQuit = () => {};
+var users = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+]
 
 document.body.addEventListener("click", function (event) {
   event.preventDefault();
@@ -17,165 +19,26 @@ document.body.addEventListener("click", function (event) {
 </script>
 
 <template>
-  <!-- Header -->
-  <div class="header">
-    <!-- navigation -->
-    <div class="nav">
-      <router-link to="/">{{ t("nav.home") }}</router-link>
-      <router-link to="/about">{{ t("nav.about") }}</router-link>
-    </div>
-    <!-- Menu -->
-    <div class="menu">
-      <div class="language">
-        <div
-          v-for="item in languages"
-          :key="item"
-          :class="{ active: item === locale }"
-          @click="onclickLanguageHandle(item)"
-          class="lang-item"
-        >
-          {{ t("languages." + item) }}
-        </div>
-      </div>
-      <div class="bar">
-        <div class="bar-btn" @click="onclickMinimise">
-          {{ t("topbar.minimise") }}
-        </div>
-        <div class="bar-btn" @click="onclickQuit">{{ t("topbar.quit") }}</div>
-      </div>
-    </div>
-  </div>
-  <!-- Page -->
-  <div class="view">
-    <router-view />
-  </div>
+
+  <body>
+    <n-layout>
+      <n-layout-header class="h-16">
+        <n-h1 prefix="bar" class="m-4">
+          Socket Chatroom
+        </n-h1>
+      </n-layout-header>
+      <n-layout has-sider>
+        <n-layout-sider>
+          <div v-for="user in users" :key="user">
+            <conversation-item-vue :name="user" />
+          </div>
+        </n-layout-sider>
+        <n-layout-content>
+          <from-conversation-vue name="Alice" msg="Hello" />
+          <to-conversation-vue name="Bob" msg="Hi" />
+        </n-layout-content>
+      </n-layout>
+    </n-layout>
+  </body>
 </template>
 
-<style lang="scss">
-@import url("./assets/css/reset.css");
-@import url("./assets/css/font.css");
-
-html {
-  width: 100%;
-  height: 100%;
-}
-body {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  font-family: "JetBrainsMono";
-  background-color: transparent;
-}
-
-#app {
-  position: relative;
-  // width: 900px;
-  // height: 520px;
-  height: 100%;
-  background-color: rgba(219, 188, 239, 0.9);
-  overflow: hidden;
-}
-.header {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  align-items: center;
-  justify-content: space-between;
-  height: 50px;
-  padding: 0 10px;
-  background-color: rgba(171, 126, 220, 0.9);
-  .nav {
-    a {
-      display: inline-block;
-      min-width: 50px;
-      height: 30px;
-      line-height: 30px;
-      padding: 0 5px;
-      margin-right: 8px;
-      background-color: #ab7edc;
-      border-radius: 2px;
-      text-align: center;
-      text-decoration: none;
-      color: #000000;
-      font-size: 14px;
-      white-space: nowrap;
-      &:hover,
-      &.router-link-exact-active {
-        background-color: #d7a8d8;
-        color: #ffffff;
-      }
-    }
-  }
-  .menu {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: space-between;
-    .language {
-      margin-right: 20px;
-      border-radius: 2px;
-      background-color: #c3c3c3;
-      overflow: hidden;
-      .lang-item {
-        display: inline-block;
-        min-width: 50px;
-        height: 30px;
-        line-height: 30px;
-        padding: 0 5px;
-        background-color: transparent;
-        text-align: center;
-        text-decoration: none;
-        color: #000000;
-        font-size: 14px;
-        &:hover {
-          background-color: #ff050542;
-          cursor: pointer;
-        }
-        &.active {
-          background-color: #ff050542;
-          color: #ffffff;
-          cursor: not-allowed;
-        }
-      }
-    }
-    .bar {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: nowrap;
-      align-items: center;
-      justify-content: flex-end;
-      min-width: 150px;
-      .bar-btn {
-        display: inline-block;
-        min-width: 80px;
-        height: 30px;
-        line-height: 30px;
-        padding: 0 5px;
-        margin-left: 8px;
-        background-color: #ab7edc;
-        border-radius: 2px;
-        text-align: center;
-        text-decoration: none;
-        color: #000000;
-        font-size: 14px;
-        &:hover {
-          background-color: #d7a8d8;
-          color: #ffffff;
-          cursor: pointer;
-        }
-      }
-    }
-  }
-}
-
-.view {
-  position: absolute;
-  top: 50px;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: hidden;
-}
-</style>
