dfffc0be2714a25f4e2789f6bc6c7e13fccba87f
cyp0633 <cyp0633@cyp0633.icu>
2022-11-24 08:42:06 +0800

    Add top bar

---
+++ frontend/src/components/TopBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {defineComponent} from 'vue';
+import {NH1} from 'naive-ui';
+defineComponent({
+    name:"TopBar",
+    props:{}
+})
+</script>
+
+<template>
+    <div class="w-full h-auto">
+        <div class="container m-4">
+            <n-h1 prefix="bar">Socket Chatroom</n-h1>
+        </div>
+    </div>
+</template>
