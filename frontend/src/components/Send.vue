3bb3ed867334ce4f4c0643dc8ecee31090744724
cyp0633 <cyp0633@cyp0633.icu>
2022-11-24 10:41:34 +0800

    Send component

---
+++ frontend/src/components/Send.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+import { NInput, NButton } from 'naive-ui'
+// defineProps<{}>()
+
+var msg = ref("")
+
+function sendMsg() {
+    console.log(msg.value)
+}
+</script>
+
+<template>
+    <div class="w-full m-4">
+        <n-input v-model:value="msg" type="textarea" placeholder="发送消息..." />
+    </div>
+    <div class="flex flex-row-reverse">
+        <n-button class="m-4" type="primary" @click="sendMsg">发送</n-button>
+    </div>
+</template>
