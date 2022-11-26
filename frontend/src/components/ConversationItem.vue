8fb8f358d597a163275ba66421d0b07d594b9512
cyp0633 <cyp0633@gmail.com>
2022-11-26 11:15:27 +0800

    Make ConversationItem clickable

--- frontend/src/components/ConversationItem.vue
+++ frontend/src/components/ConversationItem.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import { NAvatar } from 'naive-ui';
+import { NAvatar, NP } from 'naive-ui';
 const props = defineProps<{
     name: string;
 }>()
+
+function logNav() {
+    console.log(props.name)
+}
 </script>
 
 <template>
-    <div class="flex flex-row m-4">
-        <div class="flex mx-2">
-            <n-avatar round size="medium">{{ name }}</n-avatar>
+    <div class="h-20">
+        <div class="flex flex-row z-10 absolute m-4">
+            <div class="flex mx-2">
+                <n-avatar round size="medium">{{ name }}</n-avatar>
+            </div>
+            <div class="flex mx-2">
+                <n-p>{{ name }}</n-p>
+            </div>
         </div>
-        <div class="flex mx-2">
-            <slot>{{ name }}</slot>
-        </div>
+        <div class="w-full h-20 z-20 absolute" @click="$emit('navToConversation', name)" />
     </div>
 </template>
