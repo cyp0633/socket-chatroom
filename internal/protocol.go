80c892d88044f3b26a7f49e37e92479f46e6cf45
cyp0633 <cyp0633@gmail.com>
2022-11-26 22:47:13 +0800

    Fix: pull msg error

--- internal/protocol.go
+++ internal/protocol.go
@@ -35,8 +35,8 @@ func DoHelo() {
 
 	buf := make([]byte, 1024)
 	n, err := c.Read(buf) // 返回其他客户端列表
-	str := string(buf[:n])
-	if err != nil || !heloReplyRegex.MatchString(str) {
+
+	if err != nil || !heloReplyRegex.Match(buf) {
 		Logger.Error("Failed to receive HELO reply", zap.Error(err))
 	}
 
@@ -47,7 +47,7 @@ func DoHelo() {
 	Logger.Info("Received client list", zap.Strings("clients", clients))
 }
 
-var sendReplyRegex = regexp.MustCompile(`^OK.+`)
+var sendReplyRegex = regexp.MustCompile(`^OK`)
 
 func DoSend(msg string, to string) {
 	c := *conn
@@ -58,10 +58,9 @@ func DoSend(msg string, to string) {
 	}
 
 	buf := make([]byte, 1024)
-	n, err := c.Read(buf)
-	str = string(buf[:n])
-	if err != nil || !sendReplyRegex.MatchString(str) {
-		Logger.Error("Failed to receive SEND reply", zap.Error(err))
+	_, err = c.Read(buf)
+	if err != nil || !sendReplyRegex.Match(buf) {
+		Logger.Error("Failed to receive SEND reply", zap.String("msg", string(buf)), zap.Error(err))
 	}
 
 	Messages = append(Messages, Message{
@@ -76,7 +75,7 @@ var pullReplyRegex = regexp.MustCompile(`^LEN [0-9]+\n(FROM .+ CONTENT .+\n)*END
 
 var fromRegex = regexp.MustCompile(`^FROM .+ CONTENT`)
 
-var contentRegex = regexp.MustCompile(`CONTENT .+\n`)
+var contentRegex = regexp.MustCompile(`CONTENT .+`)
 
 func DoPull() {
 	c := *conn
@@ -99,8 +98,11 @@ func DoPull() {
 		if m == "END" || i == 0 || m == "" {
 			continue
 		}
-		from := fromRegex.FindString(m)[5 : len(m)-8]
-		content := contentRegex.FindString(m)[8:]
+		temp := ""
+		temp = fromRegex.FindString(m)
+		from := temp[5 : len(temp)-8]
+		temp = contentRegex.FindString(m)
+		content := temp[8:]
 		Messages = append(Messages, Message{
 			Type:    "to",
 			Content: content,
