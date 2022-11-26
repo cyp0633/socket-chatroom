12d0aa4f71d62fe3cbdbb77d2426dad1483994f1
cyp0633 <cyp0633@gmail.com>
2022-11-27 01:07:59 +0800

    Highlight current user

--- frontend/src/components/ConversationItem.vue
+++ frontend/src/components/ConversationItem.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { NAvatar, NP } from 'naive-ui';
 const props = defineProps<{
-    name: string;
+    name: string,
+    highlight?: boolean,
 }>()
 
 function logNav() {
@@ -13,10 +14,12 @@ function logNav() {
     <div class="h-20">
         <div class="flex flex-row z-10 absolute m-4">
             <div class="flex mx-2">
-                <n-avatar round size="medium">{{ name }}</n-avatar>
+                <n-avatar round size="medium" :style="highlight ? '{backgroundColor: blue}' : '{}'">
+                    {{ name }}</n-avatar>
             </div>
             <div class="flex mx-2">
-                <n-p>{{ name }}</n-p>
+                <!-- if highlight = true, n-p type=info -->
+                <n-p :type="highlight ? 'info' : ''">{{ name }}</n-p>
             </div>
         </div>
         <div class="w-full h-20 z-20 absolute" @click="$emit('navToConversation', name)" />
--- frontend/src/components/Sider.vue
+++ frontend/src/components/Sider.vue
@@ -5,6 +5,7 @@ import { reactive, ref, onMounted } from 'vue';
 import { FetchClients } from '../../wailsjs/go/main/App';
 const users = reactive([] as string[]);
 const router = useRouter();
+const currUser = ref("");
 
 onMounted(async () => {
     // fetch clients every 5 seconds
@@ -13,15 +14,20 @@ onMounted(async () => {
         const clients = await FetchClients();
         users.splice(0, users.length, ...clients);
     }, 5000);
-
 });
+
+function goToConversation(ip: string) {
+    router.push('/conversation/' + ip);
+    currUser.value = ip;
+    console.log("Current user: ", currUser.value);
+}
 
 </script>
 
 <template>
     <div class="h-full">
         <div v-for="user in users" :key="user">
-            <conversation-item-vue :name="user" @nav-to-conversation="(dest) => router.push('/conversation/' + dest)" />
+            <conversation-item-vue :name="user" @nav-to-conversation="(dest) => goToConversation(dest)" :highlight="currUser==user"/>
         </div>
     </div>
 </template>
